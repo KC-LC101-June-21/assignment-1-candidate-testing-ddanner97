2f88c810219af2893f656e115bf2c2d78b544e5c
ddanner97 <>
2021-06-04 02:52:57 +0000

    completed quiz

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,46 +1,69 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
+let candidateName
 
-// TODO 1.1a: Define candidateName // 
-let candidateName;
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = "Who was the first American woman in space? "
+let correctAnswer = "Sally Ride"
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
 
+let questions = ["1) Who was the first America woman in space?\n ",
+                "2) True or false: 5 kilometer == 5000 meter?\n ",
+                "3) (5+3)/2 * 10?\n ",
+                "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?\n ",
+                "5) What is the minimum crew size for the ISS?\n "];
+
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+
+let candidateAnswers = [];
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-
+  
+  return input.question("Enter your name: ")
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  
+  for (let i = 0; i < questions.length; i++){
+    candidateAnswers[i] = input.question(questions[i] + "Your Answer: ")
+    console.log("Correct Answer: " + correctAnswers[i] + "\n")
+  }
+  
 }
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  let correctCount = 0
 
+  for (let i = 0; i < questions.length; i++){
+    
+    if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()){
+      correctCount ++
+    }
 
-  let grade;
+  }
+
+  let grade = (correctCount / 5) * 100;
   
-
-  return grade;
+  return [grade, correctCount];
 }
 
 function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
+  // Get name and Greet
+  candidateName = askForName();
+  console.log("Hello," + candidateName + "\n")
   
+  //Ask question
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+
+  //Grade quiz
+  let grade = gradeQuiz(candidateAnswers)
+  console.log(">>> Overall Grade: " + grade[0] + "% (" + grade[1] + " of 5 responses correct) <<<")
+
+  if (grade[0] < 60){
+    console.log(">>> Status: FAILED <<<\n")
+  } else {
+    console.log(">>> Status: PASSED <<<\n")
+  }
 }
 
 // Don't write any code below this line //
