c4200daa516412da2456461ab4062616d52d72e1
ddanner97 <50763041+ddanner97@users.noreply.github.com>
2021-06-10 23:54:41 +0000

    Candidate name initialize to string

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,6 +1,6 @@
 const input = require('readline-sync');
 
-let candidateName
+let candidateName = ""
 
 let question = "Who was the first American woman in space? "
 let correctAnswer = "Sally Ride"
